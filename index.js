eb41088caad9c909bd1c209322289913f93aa934
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-11-18 18:51:58 +0530

    Intregrate socket.io

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+// socket connection
+io.on("connection", (socket) => {
+  console.log("A new user connected", socket.id);
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => {
+  console.log("Server is running on port 9000");
+});
